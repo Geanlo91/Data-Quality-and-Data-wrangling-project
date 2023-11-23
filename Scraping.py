0be3c8a3a4e3a13778ea40bc1920f91e270d8211
Geanlo91 <119528672+Geanlo91@users.noreply.github.com>
2023-11-23 22:32:18 +0200

    commit

--- Scraping.py
+++ Scraping.py
@@ -12,13 +12,13 @@ web_pages = pd.read_csv('web_pages.csv')
 for index, row in web_pages.iterrows():
     urls = row['url']
     for url in urls:
-        response = requests.get(urls)
+        response = requests.get(url)
 
         if response.status_code == 200:
             soup = BeautifulSoup(response.text,'html.parser')
 
 
-        #Extract table info with heading 'Index Performance'
+        #Extract table elements
             tables = soup.find_all('table')
 
         #iterate through each table and extract numeric data
@@ -26,8 +26,15 @@ for index, row in web_pages.iterrows():
                 #extract text content from the table
                 table_text = table.get_text()
 
-        #Print the numeric patterns found
-                print(f"Numeric data found on the page{url}:\n{table_text}\n")
+                #search for numeric data within the table text
+                match = re.search(r'(\d{1,3}(,\d{3})*(\.\d+)?)',table_text)
+
+                if match:
+                    #Print the numeric patterns found
+                    print(f"Numeric data found on the page{url}:\n{match}\n")
+
+                else:
+                    print(f"No numeric data found on the page{url}\n")
         else:
             print(f"Failed to retrieve the web page{url}.Status code:{response.status_code}")
 
