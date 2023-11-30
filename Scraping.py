9dc9948cb036740e7f960872956cf8fae39041f4
Geanlo91 <119528672+Geanlo91@users.noreply.github.com>
2023-12-01 00:25:24 +0200

    commit

--- Scraping.py
+++ Scraping.py
@@ -9,27 +9,25 @@ web_pages = pd.read_csv('web_pages.csv')
 
 
 #Loop through each URL
+def new_func(url):
+    return requests.get(url,headers={'User-Agent':'Mozilla/5.0'})
+
 for index, row in web_pages.iterrows():
     url = row['url']
     page_type = row['type']
     compliance = row['compliance']
 
-    #Check if the page is compliant
-    if not url.startswith('http'):
-        url = 'http://' + url
+    #Send HTTP request to the URL
+    response = new_func(url)
 
-    try:
-        response = requests.get(url)
-        
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as e:
-        print(f"Failed to retrieve web page.Error: {e}")
-        continue
+    #Check if the page exists
+    if response.status_code == 200:
+        #Parse the HTTP response
 
+        soup = BeautifulSoup(response.text,'html.parser')
+    
 
-    soup = BeautifulSoup(response.text,'html.parser')
-
-    if page_type == 'table':
+    if page_type == '':
         #Extract table elements
         tables = soup.find_all('table')
 
@@ -46,7 +44,7 @@ for index, row in web_pages.iterrows():
                 print(f"Numeric data found on the page{url}:\n{match}\n")
 
             else:
-                print(f"No numeric data found on the page{url}\n")
+                print(f"No numeric data found on the page{url}\n")            
     else:
         print(f"Page type {page_type} not supported")
 else:
