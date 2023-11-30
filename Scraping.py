639adf5e7573c85de30593fc266702603187d8c8
Geanlo91 <119528672+Geanlo91@users.noreply.github.com>
2023-11-30 21:37:37 +0200

    commit

--- Scraping.py
+++ Scraping.py
@@ -10,35 +10,46 @@ web_pages = pd.read_csv('web_pages.csv')
 
 #Loop through each URL
 for index, row in web_pages.iterrows():
-    urls = row['url'].split('|')
-    for url in urls:
-        #Request the page
-        response = requests.get(url)
+    url = row['url']
+    page_type = row['type']
+    compliance = row['compliance']
+
+    #Check if the page is compliant
+    if not url.startswith('http'):
+        url = 'http://' + url
+
+    try:
+        response =requests.get(url,timeout=10)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print(f"Failed to retrieve web page.Error: {e}")
+        continue
 
 
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text,'html.parser')
+    soup = BeautifulSoup(response.text,'html.parser')
 
-
+    if page_type == 'table':
         #Extract table elements
-            tables = soup.find_all('table')
+        tables = soup.find_all('table')
 
         #iterate through each table and extract numeric data
-            for table in tables:
+        for table in tables:
                 #extract text content from the table
-                table_text = table.get_text()
+            table_text = table.get_text()
 
                 #search for numeric data within the table text
-                match = re.search(r'(\d{1,3}(,\d{3})*(\.\d+)?)',table_text)
+            match = re.search(r'(\d{1,3}(,\d{3})*(\.\d+)?)',table_text)
 
-                if match:
+            if match:
                     #Print the numeric patterns found
-                    print(f"Numeric data found on the page{url}:\n{match}\n")
+                print(f"Numeric data found on the page{url}:\n{match}\n")
 
-                else:
-                    print(f"No numeric data found on the page{url}\n")
-        else:
-            print(f"Failed to retrieve the web page{url}.Status code:{response.status_code}")
+            else:
+                print(f"No numeric data found on the page{url}\n")
+    else:
+        print(f"Page type {page_type} not supported")
+else:
+    print(f"Page not found: {url}")
 
 
 
